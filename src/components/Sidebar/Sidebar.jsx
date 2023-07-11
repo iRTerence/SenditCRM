78c6d92a3eb8ce6024d4f7f24a7cf13174dc435a
iRTerence <terencetan@hotmail.ca>
2023-07-11 08:34:10 -0400

    added active and inactive colors

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import {
-  GridView as GridViewIcon,
-  AccountCircle as AccountCircleIcon,
-  Terminal as TerminalIcon,
-  Assessment as AssessmentIcon,
-  Store as StoreIcon,
-  Payments as PaymentsIcon,
-  Upload as UploadIcon,
-  NotificationsActive as NotificationsActiveIcon,
-  Settings as SettingsIcon,
-  Help as HelpIcon,
-  Logout as LogoutIcon,
-} from "@mui/icons-material";
-import logo from "../../images/mophonelogo.svg";
-import dashboard from "../../images/dashboard.svg";
-import program from "../../images/program.svg";
-import crm from "../../images/crm.svg";
-import report from "../../images/reporting.svg";
-import marketing from "../../images/marketing.svg";
-import batchservices from "../../images/batchservices.svg";
-import setting from "../../images/setting.svg";
-import helpcenter from "../../images/helpcentre.svg";
-import logout from "../../images/logout.svg";
+
+import dashboardIcon from "../../images/dashboard.svg";
+import programIcon from "../../images/program.svg";
+import crmIcon from "../../images/crm.svg";
+import reportingIcon from "../../images/reporting.svg";
+import marketingIcon from "../../images/marketing.svg";
+import batchservicesIcon from "../../images/batchservices.svg";
+import settingIcon from "../../images/setting.svg";
+import helpcenterIcon from "../../images/helpcentre.svg";
+import logoutIcon from "../../images/logout.svg";
+import dashboardIconActive from "../../images/dashboardactive.svg";
+import programIconActive from "../../images/programactive.svg";
+import crmIconActive from "../../images/crmactive.svg";
+import reportingIconActive from "../../images/reportingactive.svg";
+import marketingIconActive from "../../images/marketingactive.svg";
+import batchservicesIconActive from "../../images/batchservicesactive.svg";
+import settingIconActive from "../../images/settingactive.svg";
+import helpcenterIconActive from "../../images/helpcentreactive.svg";
+import logoutIconActive from "../../images/logoutactive.svg";
 
 import "./Sidebar.scss";
 
@@ -30,29 +26,70 @@ function Sidebar({ hide }) {
   const [activeMenuItem, setActiveMenuItem] = useState(null);
   const location = useLocation();
 
+  useEffect(() => {
+    setActiveMenuItem(location.pathname.split("/")[1]);
+  }, [location.pathname]);
+
   function handleMenuItemClick(menuItem) {
     setActiveMenuItem(menuItem);
   }
 
   const menuItems = [
-    { id: "", text: "Dashboard", icon: dashboard },
-    { id: "program", text: "Program", icon: program },
-    { id: "crm", text: "CRM", icon: crm },
-    { id: "reporting", text: "Reporting", icon: report },
-    { id: "marketing", text: "Marketing", icon: marketing },
-    { id: "batch", text: "Batch Services", icon: batchservices },
+    {
+      id: "",
+      text: "Dashboard",
+      icon: dashboardIcon,
+      activeIcon: dashboardIconActive,
+    },
+    {
+      id: "program",
+      text: "Program",
+      icon: programIcon,
+      activeIcon: programIconActive,
+    },
+    { id: "crm", text: "CRM", icon: crmIcon, activeIcon: crmIconActive },
+    {
+      id: "reporting",
+      text: "Reporting",
+      icon: reportingIcon,
+      activeIcon: reportingIconActive,
+    },
+    {
+      id: "marketing",
+      text: "Marketing",
+      icon: marketingIcon,
+      activeIcon: marketingIconActive,
+    },
+    {
+      id: "batch",
+      text: "Batch Services",
+      icon: batchservicesIcon,
+      activeIcon: batchservicesIconActive,
+    },
     {
       id: "settings",
       text: "Settings",
-      icon: setting,
+      icon: settingIcon,
+      activeIcon: settingIconActive,
       subMenu: [
-        { id: "general", text: "General", icon: HelpIcon },
+        { id: "general", text: "General", icon: helpcenterIcon },
         { id: "security", text: "Security" },
         { id: "privacy", text: "Privacy" },
       ],
     },
-    { id: "help", text: "Help Centre", icon: helpcenter, subMenu: [] },
-    { id: "logout", text: "Logout", icon: logout },
+    {
+      id: "help",
+      text: "Help Centre",
+      icon: helpcenterIcon,
+      activeIcon: helpcenterIconActive,
+      subMenu: [],
+    },
+    {
+      id: "logout",
+      text: "Logout",
+      icon: logoutIcon,
+      activeIcon: logoutIconActive,
+    },
   ];
 
   return (
@@ -68,9 +105,14 @@ function Sidebar({ hide }) {
               <Link to={`/${item.id}`} style={{ textDecoration: "none" }}>
                 <div className="icon">
                   {typeof item.icon === "string" ? (
-                    <img src={item.icon} alt={item.text} />
+                    <img
+                      src={
+                        activeMenuItem === item.id ? item.activeIcon : item.icon
+                      }
+                      alt={item.text}
+                    />
                   ) : (
-                    React.createElement(item.icon)
+                    item.icon
                   )}
                 </div>
                 <span>{item.text}</span>
@@ -80,7 +122,11 @@ function Sidebar({ hide }) {
                   {item.subMenu.map((subItem) => (
                     <li key={subItem.id}>
                       <div className="icon">
-                        {subItem.icon && React.createElement(subItem.icon)}
+                        {subItem.icon && typeof subItem.icon === "string" ? (
+                          <img src={subItem.icon} alt={subItem.text} />
+                        ) : (
+                          subItem.icon
+                        )}
                       </div>
                       <Link
                         to={`/${item.id}/${subItem.id}`}
